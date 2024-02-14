43444bc5cf7062ea9aadb9b915c899cc9c1c6dd1
willprince <46878606+willprince@users.noreply.github.com>
2024-02-14 10:25:14 -0500

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -17,19 +17,7 @@ export function Navbar() {
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2"
-                >
-                  {name}
-                </Link>
-              );
-            })}
-          </div>
+          
         </nav>
       </div>
     </aside>
